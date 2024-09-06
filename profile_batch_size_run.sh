661df8429fab006edf62f66fa83fc166b022737e
duihuhu <duihuhu@163.com>
2024-09-06 14:46:21 +0800

    add sample config

--- profile_batch_size_run.sh
+++ profile_batch_size_run.sh
@@ -4,7 +4,7 @@ sleep 5
 python scripts/inference.py  scripts/inference.py configs/opensora-v1-2/inference/sample_config.py   --num-frames 2s --resolution 360p   --layernorm-kernel False --flash-attn False --batch-size 2 --prompt ["a beautiful waterfall", "a beautiful waterfall"]
 sleep 5
 
-python scripts/inference.py  scripts/inference.py configs/opensora-v1-2/inference/sample_config.py   --num-frames 2s --resolution 360p   --layernorm-kernel False --flash-attn False --batch-size 4 --prompt ["a beautiful waterfall", "a beautiful waterfall", "a beautiful waterfall", "a beautiful waterfall", "a beautiful waterfall"]
+python scripts/inference.py  scripts/inference.py configs/opensora-v1-2/inference/sample_config.py   --num-frames 2s --resolution 360p   --layernorm-kernel False --flash-attn False --batch-size 4 --prompt ["a beautiful waterfall", "a beautiful waterfall", "a beautiful waterfall", "a beautiful waterfall"]
 sleep 5
 
 
