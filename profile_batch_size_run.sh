98f41fb53ff4354db8bb1204350c5fbf19e53acf
duihuhu <duihuhu@163.com>
2024-09-07 20:43:25 +0800

    add sample config

--- profile_batch_size_run.sh
+++ profile_batch_size_run.sh
@@ -1,11 +1,11 @@
-torchrun --nproc_per_node 4 scripts/inference.py configs/opensora-v1-2/inference/sample_config.py   --num-frames 2s --resolution 480p   --layernorm-kernel False --flash-attn False --batch-size 1 --prompt ["a beautiful waterfall"]
+torchrun --nproc_per_node 2 scripts/inference.py configs/opensora-v1-2/inference/sample_config.py   --num-frames 2s --resolution 480p   --layernorm-kernel False --flash-attn False --batch-size 1 --prompt ["a beautiful waterfall"]
 sleep 5
 
-torchrun --nproc_per_node 4 scripts/inference.py  configs/opensora-v1-2/inference/sample_config.py   --num-frames 2s --resolution 480p   --layernorm-kernel False --flash-attn False --batch-size 2 --prompt ["a beautiful waterfall", "a beautiful waterfall"]
+torchrun --nproc_per_node 2 scripts/inference.py  configs/opensora-v1-2/inference/sample_config.py   --num-frames 2s --resolution 480p   --layernorm-kernel False --flash-attn False --batch-size 2 --prompt ["a beautiful waterfall", "a beautiful waterfall"]
 sleep 5
 
-torchrun --nproc_per_node 4 scripts/inference.py configs/opensora-v1-2/inference/sample_config.py   --num-frames 2s --resolution 480p   --layernorm-kernel False --flash-attn False --batch-size 4 --prompt ["a beautiful waterfall", "a beautiful waterfall", "a beautiful waterfall", "a beautiful waterfall"]
+torchrun --nproc_per_node 2 scripts/inference.py configs/opensora-v1-2/inference/sample_config.py   --num-frames 2s --resolution 480p   --layernorm-kernel False --flash-attn False --batch-size 4 --prompt ["a beautiful waterfall", "a beautiful waterfall", "a beautiful waterfall", "a beautiful waterfall"]
 sleep 5
 
 
-torchrun --nproc_per_node 4 scripts/inference.py configs/opensora-v1-2/inference/sample_config.py   --num-frames 2s --resolution 480p   --layernorm-kernel False --flash-attn False --batch-size 8 --prompt ["a beautiful waterfall", "a beautiful waterfall", "a beautiful waterfall", "a beautiful waterfall", "a beautiful waterfall", "a beautiful waterfall", "a beautiful waterfall", "a beautiful waterfall"]
+torchrun --nproc_per_node 2 scripts/inference.py configs/opensora-v1-2/inference/sample_config.py   --num-frames 2s --resolution 480p   --layernorm-kernel False --flash-attn False --batch-size 8 --prompt ["a beautiful waterfall", "a beautiful waterfall", "a beautiful waterfall", "a beautiful waterfall", "a beautiful waterfall", "a beautiful waterfall", "a beautiful waterfall", "a beautiful waterfall"]
