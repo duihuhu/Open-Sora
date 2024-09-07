f28ba150b663cf05102129193230495a6614bf36
duihuhu <duihuhu@163.com>
2024-09-07 19:40:52 +0800

    add sample config

--- profile_parallel_size_run.sh
+++ profile_parallel_size_run.sh
@@ -1,11 +1,11 @@
-python scripts/inference.py configs/opensora-v1-2/inference/sample_config.py   --num-frames 2s --resolution 360p   --layernorm-kernel False --flash-attn False --batch-size 1 --prompt ["a beautiful waterfall"]
+python scripts/inference.py configs/opensora-v1-2/inference/sample_config.py   --num-frames 2s --resolution 240p   --layernorm-kernel False --flash-attn False --batch-size 1 --prompt ["a beautiful waterfall"]
 sleep 5
 
-torchrun --nproc_per_node 2 scripts/inference.py configs/opensora-v1-2/inference/sample_config.py   --num-frames 2s --resolution 360p   --layernorm-kernel False --flash-attn False --batch-size 1 --prompt ["a beautiful waterfall"]
+torchrun --nproc_per_node 2 scripts/inference.py configs/opensora-v1-2/inference/sample_config.py   --num-frames 2s --resolution 240p   --layernorm-kernel False --flash-attn False --batch-size 1 --prompt ["a beautiful waterfall"]
 sleep 5
 
-torchrun --nproc_per_node 4 scripts/inference.py configs/opensora-v1-2/inference/sample_config.py   --num-frames 2s --resolution 360p   --layernorm-kernel False --flash-attn False --batch-size 1 --prompt ["a beautiful waterfall"]
+torchrun --nproc_per_node 4 scripts/inference.py configs/opensora-v1-2/inference/sample_config.py   --num-frames 2s --resolution 240p   --layernorm-kernel False --flash-attn False --batch-size 1 --prompt ["a beautiful waterfall"]
 sleep 5
 
 
-torchrun --nproc_per_node 8 scripts/inference.py configs/opensora-v1-2/inference/sample_config.py   --num-frames 2s --resolution 360p   --layernorm-kernel False --flash-attn False --batch-size 1 --prompt ["a beautiful waterfall"]
+torchrun --nproc_per_node 8 scripts/inference.py configs/opensora-v1-2/inference/sample_config.py   --num-frames 2s --resolution 240p   --layernorm-kernel False --flash-attn False --batch-size 1 --prompt ["a beautiful waterfall"]
