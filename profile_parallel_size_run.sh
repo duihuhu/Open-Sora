b948259a26d72b2e78d61ccbd483a3383557eddd
duihuhu <duihuhu@163.com>
2024-09-09 10:04:14 +0800

    add sample config

--- profile_parallel_size_run.sh
+++ profile_parallel_size_run.sh
@@ -1,4 +1,4 @@
-python scripts/inference.py configs/opensora-v1-2/inference/sample_config.py   --num-frames 2s --resolution 480p   --layernorm-kernel False --flash-attn False --batch-size 1 --prompt ["a beautiful waterfall"]
+python scripts/inference.py configs/opensora-v1-2/inference/sample_config.py   --num-frames 2s --resolution 144p    --layernorm-kernel False --flash-attn False --batch-size 1 --prompt ["a beautiful waterfall"]
 sleep 5
 
 torchrun --nproc_per_node 2 scripts/inference.py configs/opensora-v1-2/inference/sample_config.py   --num-frames 2s --resolution 144p   --layernorm-kernel False --flash-attn False --batch-size 1 --prompt ["a beautiful waterfall"]
